dc7661de9d86b1259ffec774c5b7bc195197ca20
JuliaMukh <mukhina_ur@mail.ru>
2025-09-17 19:52:47 +0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 export default defineConfig({
+  base:"/test/",
   plugins: [
     Inspect(),
     tailwindcss(),
